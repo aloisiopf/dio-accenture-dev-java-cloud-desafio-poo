511afa97ac7707a5e35a3259b05a79a15a279752
aloisiopf <aloisiopf@gmail.com>
2026-02-26 21:00:48 -0300

    Criação das classes (POJO) Curso e Mentoria

---
+++ src/br/eti/aloisio/dio/accenture/dominio/Curso.java
@@ -0,0 +1,30 @@
+package br.eti.aloisio.dio.accenture.dominio;
+
+public class Curso {
+    private String titulo;
+    private String descricao;
+    private int cargoHoraria;
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public int getCargoHoraria() {
+        return cargoHoraria;
+    }
+    public void setCargoHoraria(int cargoHoraria) {
+        this.cargoHoraria = cargoHoraria;
+    }
+    @Override
+    public String toString() {
+        return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargoHoraria=" + cargoHoraria + "]";
+    }
+    
+}
