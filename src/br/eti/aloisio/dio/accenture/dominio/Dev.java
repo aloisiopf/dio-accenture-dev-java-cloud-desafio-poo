4c814b3451834bb385ecbe10cc5fe680b24720bc
aloisiopf <aloisiopf@gmail.com>
2026-02-27 15:06:34 -0300

    Implementação dos métodos: inscreverBootcamp, progredir e calcularTotalXP da classe Dev

--- src/br/eti/aloisio/dio/accenture/dominio/Dev.java
+++ src/br/eti/aloisio/dio/accenture/dominio/Dev.java
@@ -1,6 +1,7 @@
 package br.eti.aloisio.dio.accenture.dominio;
 
 import java.util.LinkedHashSet;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -9,15 +10,25 @@ public class Dev {
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcamp bootcamp){
-
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
     }
 
     public void progredir() {
-
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
     }
 
     public double calcularTotalXp() {
-        return 0.0;
+        return this.conteudosConcluidos
+                .stream()
+                .mapToDouble(Conteudo::calcularXp)
+                .sum();
     }
 
     public String getNome() {
